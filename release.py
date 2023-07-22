477cf5e93d8076b771faaf7feb60f2945f716816
AleksanderZhk <bugattiforever@gmail.com>
2023-07-23 01:51:25 +0300

    pic4

--- release.py
+++ release.py
@@ -1,23 +1,27 @@
 import subprocess
 
+def git_subtree_pull():
+    # Pull the latest changes from the remote gh-pages branch into the site folder
+    pull_command = ["git", "subtree", "pull", "--prefix", "site", "origin", "gh-pages"]
+    subprocess.run(pull_command)
+
+
 def git_subtree_push():
-    command = ["git", "subtree", "push", "--prefix", "site", "origin", "gh-pages"]
-    try:
-        # Run the command using subprocess
-        result = subprocess.run(command, capture_output=True, text=True, check=True)
+    # Call the pull function before pushing
+    git_subtree_pull()
 
-        # Check the command output and handle it as needed
-        if result.stdout:
-            print("Command Output:")
-            print(result.stdout)
+    # Push the changes to the remote repository
+    push_command = ["git", "subtree", "push", "--prefix", "site", "origin", "gh-pages"]
+    push_result = subprocess.run(push_command, capture_output=True, text=True)
 
-        if result.stderr:
-            print("Error Output:")
-            print(result.stderr)
+    # Print the push command output
+    print("Push Command Output:")
+    print(push_result.stdout)
 
-    except subprocess.CalledProcessError as e:
-        print("Error occurred while executing the command:")
-        print(e.stderr)
+    # Print the error output if there's any
+    if push_result.stderr:
+        print("Error Output:")
+        print(push_result.stderr)
 def mkdocs_build():
     command = ["mkdocs", "build"]
     try:
